46d2c7692bf23117d7fc3d20e8416e84a9e778f5
Chante van der Walt <chantevdwalt87@gmail.com>
2020-05-13 13:43:17 +0200

    Delete treehouse.py

--- treehouse.py
+++
@@ -1,7 +0,0 @@
-#Find the len of the provided student_gpas list and assign it to a variable called length.
-
-student_gpas = [4.0, 2.3, 3.5, 3.7, 3.9, 2.8, 1.5, 4.0]
-lenght = len(student_gpas)
-print(lenght)
-
-#They say my code is wrong?
