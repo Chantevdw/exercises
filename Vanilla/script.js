e7999276c26d2dda9fa752a84c4788b92af181c5
Chante van der Walt <chantevdwalt87@gmail.com>
2020-05-28 18:20:39 +0200

    Merged script_2.js with script.js

--- Vanilla/script.js
+++ Vanilla/script.js
@@ -44,3 +44,48 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    addMarker();
+})
+
+var center = new google.maps.LatLng(0,0);
+
+var mapOptions = {
+    zoom: 3,
+    center: center,
+};
+
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+function addMarker() {
+
+    // DOM event listener for the center map form submit
+    google.maps.event.addDomListener(document.getElementById('mapForm'), 'submit', function(e) {
+        e.preventDefault();
+
+        // Get lat and lng values from input fields
+        var lat = document.getElementById('lat').value;
+        var lng = document.getElementById('lng').value;
+
+        // Replace input values
+        document.getElementById('lat').value = lat;
+        document.getElementById('lng').value = lng;
+
+        // Create LatLng object
+        var mapCenter = new google.maps.LatLng(lat, lng);
+
+        new google.maps.Marker({
+
+            position: mapCenter,
+            title: 'Marker',
+            map: map
+        });
+
+        // Center map
+        map.setCenter(mapCenter);
+    });
+}
+
+addMarker();
+
