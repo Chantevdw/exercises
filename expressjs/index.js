8f9ed8894293a417108bafce8de8d9f1aa85dfb1
Richard Levey <riclev123@gmail.com>
2020-06-03 20:03:02 +0200

    Post login

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,10 +1,34 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('index'));
+
+
+app.post('/login', function(request, response) {
+    const validUsername = "Chante";
+    const validPassword = "Password";
+
+    const username = request.body.username;
+    const password = request.body.password;
+
+    if (username === validUsername && password === validPassword) {
+        response.redirect('/home');
+        response.end();
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
