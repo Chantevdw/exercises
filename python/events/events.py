8921c2b5cac3ea759c7d3ddaa326443de4e31983
Chante van der Walt <chantevdwalt87@gmail.com>
2020-06-03 19:49:21 +0200

    not sure

--- python/events/events.py
+++ python/events/events.py
@@ -1,23 +1,32 @@
+#pseudo code
+# 1. Isolate elements from list
+# 2. Count elements occurance
+# 3. Store element as key and occurance_number as value
+# 4. Sort list
+
+from collections import Counter
+
 eventspath = "events.log"
 with open(eventspath) as file:
     records = {}
-    index = 10
     for row in file:
         row = row.rstrip('\n').split(" ")
         # print(row[2])
-        records[row[2]] = 1
-        #records = row.count('8')
-        records = {x:row[2].count(x) for x in row[2]}
-        #print(records)
-        sorted_list = sorted(records.items(), key=lambda i:i[1], reverse=True)
-        # for y in sorted_list:
-        #     print(sorted_list[:11])
-        print(len(sorted_list))
-        #break
+        #records[row[2]] = 1
+        results = Counter(row[2]).most_common(10)
+        print(results)
 
-        # records[row[2]] = 1
-        # print(records)
-        # break
+        # records = {x:row[2].count(x) for x in row[2]}
+        # #print(records)
+        # sorted_list = sorted(records.items(), key=lambda i:i[1], reverse=True)
+        # # for y in sorted_list:
+        # #     print(sorted_list[:11])
+        # print(len(sorted_list))
+        # #break
+        #
+        # # records[row[2]] = 1
+        # # print(records)
+        # # break
 
 
 
