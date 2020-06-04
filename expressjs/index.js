9c449f6ce8101cd7d891f69462f275adc3adf98f
Richard Levey <riclev123@gmail.com>
2020-06-04 18:38:18 +0200

    Added some hopefully helpful comments

--- expressjs/index.js
+++ expressjs/index.js
@@ -10,11 +10,25 @@ app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+/**
+ * Our default site
+ */
 app.get('/', (req, res) => res.render('index'));
 
+// here you have created the same route as above
+// there are two GET '/' routes
+// look where the login api is trying to go to for a hint
+
+/**
+ * Our home page
+ */
 app.get('/', (req, res) => res.render('home'));
 
-
+/**
+ * API Login call
+ * This redirects to our home page
+ */
 app.post('/login', function(request, response) {
     const validUsername = "Chante";
     const validPassword = "Password";
